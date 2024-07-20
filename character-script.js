ff63c54e48b5a4c29c9bca098e7a7591957e022f
forge34 <28455546+forge34@users.noreply.github.com>
2024-07-21 00:07:51 +0300

    feat : update character script

--- character-script.js
+++ character-script.js
@@ -1,6 +1,6 @@
 const Characters = require("./models/character-model");
 
-async function createCharacte(name, x, y, height, width) {
+async function createCharacte(name, x, y, width, height) {
   const character = new Characters({
     name: name,
     x: x,
@@ -15,9 +15,51 @@ async function createCharacte(name, x, y, height, width) {
 }
 
 (async () => {
-  await createCharacte("leonardo", 762, 767, 59, 30);
-  await createCharacte("goku", 811, 1166, 89, 51);
-  await createCharacte("guitar guy", 1123, 1388, 110, 100);
-  await createCharacte("mojo jojo", 278, 844, 66, 55);
-  await createCharacte("jason voorhees", 698, 661, 70, 55);
+  // await createCharacte("leonardo", 762, 767, 59, 30);
+  // await createCharacte("goku", 811, 1166, 89, 51);
+  // await createCharacte("guitar guy", 1123, 1388, 110, 100);
+  // await createCharacte("mojo jojo", 278, 844, 66, 55);
+  // await createCharacte("jason voorhees", 698, 661, 70, 55);
+  //
+  const leo = [(1152 / 1920).toFixed(2), (1078 / 2689).toFixed(2)];
+  const goku = [(1230 / 1920).toFixed(2), (1684 / 2689).toFixed(2)];
+  const guitar = [(1713 / 1920).toFixed(2), (2048 / 2689).toFixed(2)];
+  const mojo = [(424 / 1920).toFixed(2), (1197 / 2689).toFixed(2)];
+  const jason = [(1061 / 1920).toFixed(2), (915 / 2689).toFixed(2)];
+
+  await createCharacte(
+    "leonardo",
+    leo[0],
+    leo[1],
+    (45 / 1920).toFixed(2),
+    (90 / 2689).toFixed(2),
+  );
+  await createCharacte(
+    "goku",
+    goku[0],
+    goku[1],
+    (45 / 1920).toFixed(2),
+    (93 / 2689).toFixed(2),
+  );
+  await createCharacte(
+    "guitar guy",
+    guitar[0],
+    guitar[1],
+    (84 / 1920).toFixed(2),
+    (86 / 2689).toFixed(2),
+  );
+  await createCharacte(
+    "mojo jojo",
+    mojo[0],
+    mojo[1],
+    (44 / 1920).toFixed(2),
+    (60 / 2689).toFixed(2),
+  );
+  await createCharacte(
+    "jason voorhees",
+    jason[0],
+    jason[1],
+    (44 / 1920).toFixed(2),
+    (63 / 2689).toFixed(2),
+  );
 })();
