bd0199942d662ef58c2f52a4f1a9bf6a7dc74521
forge34 <28455546+forge34@users.noreply.github.com>
2024-07-21 00:08:10 +0300

    feat : normalize coordinates for different screens

--- controllers/game-controller.js
+++ controllers/game-controller.js
@@ -3,7 +3,6 @@ const CharacterModel = require("../models/character-model");
 
 module.exports.start = (req, res) => {
   req.session.startTime = Date.now();
-  console.log(req.session);
   res.json("test");
 };
 
@@ -18,23 +17,31 @@ module.exports.check = [
   body("name").trim().escape(),
   async (req, res) => {
     const body = req.body;
-    console.log(req.body);
+    const x = body.x;
+    const y = body.y;
+
     const character = await CharacterModel.findOne()
       .where("name")
       .equals(body.name)
       .exec();
+
+    const newX = character.x * req.body.width;
+    const newY = character.y * req.body.height;
+    const newWidth = character.width * req.body.width;
+    const newHeight = character.height * req.body.height;
+
     if (character === null) res.json("Character not found");
     else if (
-      body.x >= character.x &&
-      body.x <= character.x + character.width &&
-      body.y >= character.y &&
-      body.y <= character.y + character.height
+      x >= newX &&
+      x <= newX + newWidth &&
+      y >= newY &&
+      y <= newY + newHeight
     ) {
       res.status(200).json({
         message: "Character found",
         found: true,
-        x: character.x + character.width / 2.3,
-        y: character.y + character.height / 2.3,
+        x: newX + newWidth / 2,
+        y: newY + newHeight / 2,
       });
     } else {
       res.status(200).json({ message: "not correct", found: false });
